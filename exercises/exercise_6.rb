0851eb6c090a8de3cc3a857a8ff19c9beba9f577
Camilo Gomez <camilogomezmusic@gmail.com>
2020-09-14 14:42:55 -0400

    Complete ex. 6

--- exercises/exercise_6.rb
+++ exercises/exercise_6.rb
@@ -9,3 +9,11 @@ puts "Exercise 6"
 puts "----------"
 
 # Your code goes here ...
+@store1.employees.create(first_name: "Khurram", last_name: "Virani", hourly_rate: 60)
+@store1.employees.create(first_name: "Francisco", last_name: "Maturana", hourly_rate: 20)
+@store1.employees.create(first_name: "Bolillo", last_name: "Gomez", hourly_rate: 18)
+@store2.employees.create(first_name: "Pecoso", last_name: "Castro", hourly_rate: 15)
+@store2.employees.create(first_name: "Jose", last_name: "Pekerman", hourly_rate: 60)
+
+#  Employee.all.each { |x| puts x.first_name, x.last_name, x.store_id }
+
